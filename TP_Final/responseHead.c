01c5dd22f23b305c25b0a797f1e49537e172b4a4
Macarena Belén Garcia Arcija <belugarciaarcija@gmail.com>
2017-07-02 19:00:25 -0300

    Content-type
    Ver si el content-type anda(es lo que esta comentado) En teoria pone el tipo mime de los distintos tipos de archivos en la cabecera del response

--- TP_Final/responseHead.c
+++ TP_Final/responseHead.c
@@ -16,36 +16,39 @@ int Output_HTTP_Headers(int conn, struct ReqInfo * reqinfo) {
     Writeline(conn, buffer, strlen(buffer));
 
     Writeline(conn, "Server: PGWebServ v0.1\r\n", 24);
-    Writeline(conn, ("Content-Type: text/html\r\n"), 25);
-//	Writeline(conn, ("Content-Type: %s\r\n", ), 25);
+    Writeline(conn, "Content-Type: text/html\r\n", 25);
+//	
+//recorre el reqinfo->resource hasta que llega al .tipo_de_archivo
+//entonces manda ese content type 
+/*
+ * //Se define el tipo de contenido de la respuesta 
+ * //Recorriendo el reqinfo->resource hasta que llega al .tipo_de_archivo
+ * 
+ * if ((strstr(reqinfo->resource, ".html"))||( strstr(reqinfo->resource, ".htm") )){//si el recurso es .html o .htm
+ * 		Writeline(conn, "Content-Type: text/html\r\n", 25);// entonces el Content-Type vale text/html
+ * 
+ * }else if(strstr(reqinfo->resource, ".txt")){//si el recurso es .txt
+ * 		Writeline(conn, "Content-Type: text/plain\r\n", 25);//entonces el Content-Type vale text/plain
+ * 
+ * }else if(strstr(reqinfo->resource, ".gif")){//si el recurso es .gif
+ * 		Writeline(conn, "Content-Type: image/gif\r\n", 25);//entonces el Content-Type vale image/gif
+ * 
+ * }else if((strstr(reqinfo->resource, ".jpg"))||( strstr(reqinfo->resource, ".jpeg") )){//si el recurso es .jpg o .jpeg
+ * 		Writeline(conn, "Content-Type: image/jpeg\r\n", 25);//entonces el Content-Type vale image/jpeg
+ * 
+ * }else if(strstr(reqinfo->resource, ".pdf")){//si el recurso es .pdf
+ * 		Writeline(conn, "Content-Type: application/pdf\r\n", 25);//entonces el Content-Type vale application/pdf
+ * 
+ * }else {//si el recurso no es ninguno de estos tipos
+ * 		Writeline(conn, "Content-Type: application/octet-stream\r\n", 25);//entonces el Content-Type vale application/octet-stream
+ * } 
+ * 
+ * */
+
+
+//Poner el Content-length
 
     Writeline(conn, "\r\n", 2);
 
     return 0;
 }
-/*HACER QUE ESCRIBA LA CABECERA DEL TIPO MIME*/
-/*
- * FUNCION QUE RETORNA EL TIPO DE CONTENT TYPE
- * 
- * 
- * char *get_mime_type(char *directorio_de_archivo){
-	char *ext = strrchr(directorio_de_archivo, '.');
-	if (!ext) return NULL;
-	if (strcmp(ext, ".html") == 0 || strcmp(ext, ".htm") == 0) return "text/html";
-	if (strcmp(ext, ".jpg") == 0 || strcmp(ext, ".jpeg") == 0) return "image/jpeg";
-	if (strcmp(ext, ".gif") == 0) return "image/gif";
-	if (strcmp(ext, ".png") == 0) return "image/png";
-	if (strcmp(ext, ".css") == 0) return "text/css";
-	if (strcmp(ext, ".au") == 0) return "audio/basic";
-	if (strcmp(ext, ".wav") == 0) return "audio/wav";
-	if (strcmp(ext, ".avi") == 0) return "video/x-msvideo";
-	if (strcmp(ext, ".mpeg") == 0 || strcmp(ext, ".mpg") == 0) return "video/mpeg";
-	if (strcmp(ext, ".mp3") == 0) return "audio/mpeg";
-	return NULL;
-}
-
- * 
- * 
- * 
- * */
-
