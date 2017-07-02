14d23fff0deede310379f93f435625703a942dec
Macarena Belén Garcia Arcija <belugarciaarcija@gmail.com>
2017-07-02 19:56:24 -0300

    °-°

--- TP_Final/responseHead.c
+++ TP_Final/responseHead.c
@@ -15,8 +15,8 @@ int Output_HTTP_Headers(int conn, struct ReqInfo * reqinfo) {
     sprintf(buffer, "HTTP/1.0 %d OK\r\n", reqinfo->status);
     Writeline(conn, buffer, strlen(buffer));
 
-    Writeline(conn, "Server: PGWebServ v0.1\r\n", 24);
-    Writeline(conn, "Content-Type: text/html\r\n", 25);
+    Writeline(conn, "Server: PGWebServ v0.1\r\n", 27);
+    Writeline(conn, "Content-Type: text/html\r\n", 28);
 //	
 //recorre el reqinfo->resource hasta que llega al .tipo_de_archivo
 //entonces manda ese content type 
@@ -25,22 +25,22 @@ int Output_HTTP_Headers(int conn, struct ReqInfo * reqinfo) {
  * //Recorriendo el reqinfo->resource hasta que llega al .tipo_de_archivo
  * 
  * if ((strstr(reqinfo->resource, ".html"))||( strstr(reqinfo->resource, ".htm") )){//si el recurso es .html o .htm
- * 		Writeline(conn, "Content-Type: text/html\r\n", 25);// entonces el Content-Type vale text/html
+ * 		Writeline(conn, "Content-Type: text/html\r\n", 28);// entonces el Content-Type vale text/html
  * 
  * }else if(strstr(reqinfo->resource, ".txt")){//si el recurso es .txt
- * 		Writeline(conn, "Content-Type: text/plain\r\n", 25);//entonces el Content-Type vale text/plain
+ * 		Writeline(conn, "Content-Type: text/plain\r\n", 29);//entonces el Content-Type vale text/plain
  * 
  * }else if(strstr(reqinfo->resource, ".gif")){//si el recurso es .gif
- * 		Writeline(conn, "Content-Type: image/gif\r\n", 25);//entonces el Content-Type vale image/gif
+ * 		Writeline(conn, "Content-Type: image/gif\r\n", 28);//entonces el Content-Type vale image/gif
  * 
  * }else if((strstr(reqinfo->resource, ".jpg"))||( strstr(reqinfo->resource, ".jpeg") )){//si el recurso es .jpg o .jpeg
- * 		Writeline(conn, "Content-Type: image/jpeg\r\n", 25);//entonces el Content-Type vale image/jpeg
+ * 		Writeline(conn, "Content-Type: image/jpeg\r\n", 29);//entonces el Content-Type vale image/jpeg
  * 
  * }else if(strstr(reqinfo->resource, ".pdf")){//si el recurso es .pdf
- * 		Writeline(conn, "Content-Type: application/pdf\r\n", 25);//entonces el Content-Type vale application/pdf
+ * 		Writeline(conn, "Content-Type: application/pdf\r\n", 38);//entonces el Content-Type vale application/pdf
  * 
  * }else {//si el recurso no es ninguno de estos tipos
- * 		Writeline(conn, "Content-Type: application/octet-stream\r\n", 25);//entonces el Content-Type vale application/octet-stream
+ * 		Writeline(conn, "Content-Type: application/octet-stream\r\n", 42);//entonces el Content-Type vale application/octet-stream
  * } 
  * 
  * */
